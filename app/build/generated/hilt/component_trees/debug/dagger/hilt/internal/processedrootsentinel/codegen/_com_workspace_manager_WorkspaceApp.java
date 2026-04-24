5574a59f2daeb426e0d63e68e2f98394a2bb986d
Hasan <hasan@inverseai.com>
2026-04-25 00:27:31 +0600

    update git ignored file

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_workspace_manager_WorkspaceApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.workspace.manager.WorkspaceApp"
-)
-public final class _com_workspace_manager_WorkspaceApp {
-}
