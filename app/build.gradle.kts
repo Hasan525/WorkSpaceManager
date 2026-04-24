31c585e3d2412feef38ba2c58efcfa436970040f
Hasan <hasan@inverseai.com>
2026-04-24 21:30:52 +0600

    remove comment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,7 +92,6 @@ dependencies {
     implementation("androidx.hilt:hilt-work:1.2.0")
     ksp("androidx.hilt:hilt-compiler:1.2.0")
 
-    // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
